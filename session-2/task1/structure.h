f2237184e9746730321e7a2c7448f11c1bfbb16e
Amrit183-cpu <rhrt0563@leeds.ac.uk>
2026-03-20 09:53:13 +0000

    2.8 s2 t1

---
+++ session-2/task1/structure.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+#ifndef structure
+#define structure
+
+struct mystructure {
+    int rand_var = 0;
+};
+
+#endif
---
+++ session-2/task1/student.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+#ifndef student
+#define student
+
+void myfunction () {
+    printf("I am located in student.h");
+};
+
+#endif
